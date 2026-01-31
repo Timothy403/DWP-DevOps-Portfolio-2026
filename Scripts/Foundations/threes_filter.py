3183603c1337cb6d56ae1ba3d08e43699418113a
Timothy Thirry <thirrytimothy@gmail.com>
2026-01-31 17:55:55 +0000

    Update threes_filter.py

--- Scripts/Foundations/threes_filter.py
+++ Scripts/Foundations/threes_filter.py
@@ -1,1 +1,10 @@
+def get_threes(numbers):
+    threes_list = []
 
+    # Loop through the numbers
+    for num in numbers:
+        # Check if the number is divisible by 3
+        if num % 3 == 0:
+            threes_list.append(num)
+
+    return threes_list
