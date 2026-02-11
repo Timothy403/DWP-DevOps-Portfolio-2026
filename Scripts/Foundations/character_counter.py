12b12024d4b0a65aa94992a69c57624d1fc410cc
Timothy Thirry <thirrytimothy@gmail.com>
2026-02-11 15:02:18 +0000

    Update character_counter.py

--- Scripts/Foundations/character_counter.py
+++ Scripts/Foundations/character_counter.py
@@ -1,12 +1,8 @@
-def count_characters(text):
+def count_characters(text=""):
+    # Providing a default "" prevents a crash if no argument is passed
     character_dict = {}
+    
     for character in text:
-        if character not in character_dict:
-            character_dict[character] = 1
-        else:
-            character_dict[character] += 1
+        character_dict[character] = character_dict.get(character, 0) + 1
 
     return character_dict
-
-
-count_characters("banana")
